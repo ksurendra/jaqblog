c50cc5b6347840d863f27af6bbe7e18a29f10e76
Suren Konathala <konathalasuren@gmail.com>
2018-11-27 14:11:03 -0600

    Initial draft

---
+++ ui.resources/src/config/cosmo.config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    production: false,
+    read_key: 'cnvZLCo9wIhlRH3UJSozZHTGzLRdJvNlttaXik25eOzCjJrvaq',
+    write_key: 'BjXABUhnIpcRxDONcQVrIY9MA8eqNGPZnpPxgbXwora0egi3M3',
+    bucket_id: '5bfd688dd6977b5de6ef7dc1',
+    bucket_slug: 'jaqblog', 
+    URL: 'https://api.cosmicjs.com/v1/',
+    };
+    
